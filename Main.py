0003fe2600d0f98acbe7c6417af395e72f3c1649
Rene Cruz <renecruz.0711@gmail.com>
2023-09-18 00:12:50 -0600

    Comment code for better explanation

--- Main.py
+++ Main.py
@@ -4,36 +4,49 @@ import pickle
 import cvzone
 
 # ? Video feed
-cap = cv.VideoCapture('parking.mp4')
+capture = cv.VideoCapture('parking.mp4')
+#? size values
 width, height = 65, 30
 
+#? call previous file created with 'ParkingSpacePicker.py'
 with open('CarParkPos', 'rb') as f:
-    posList = pickle.load(f)
+    positionList = pickle.load(f)
 
+#? print green or red rectangles to show free spaces
+def checkParkingSpace(imgPro):
+    #? iterate the selected rectangles by position
+    for position in positionList:
+        x, y = position
+        #? separate rectangles by creating a new and only file
+        imgCrop = imgPro[y:y+height, x:x+width]
+        #? count -> binary count for the free space separation
+        count = cv.countNonZero(imgCrop)
+        cvzone.putTextRect(img, str(count), (x, y+height-10), scale=0.8, thickness=1, offset=0)
+        #? categorize empty and non-empty space
+        if count < 430:
+            color = (0,255,0) #? green
+        else:
+            color = (0, 0, 255) #? red
+        #? show rectangle in the selected color
+        cv.rectangle(img, position, (position[0]+width, position[1]+height), color , 2)
 
-def checkParkingSpace():
-    for pos in posList:
-        x, y = pos
-        # imgCrop = img[y:y+height, x:x+width]
-        # cv.imshow(str(x*y), imgCrop)
-
-
+#? Base video
 while True:
-    if cap.get(cv.CAP_PROP_POS_FRAMES) == cap.get(cv.CAP_PROP_FRAME_COUNT):
-        cap.set(cv.CAP_PROP_POS_FRAMES, 0)
-    success, img = cap.read()
-
-    imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    imgBlur = cv.GaussianBlur(imgGray, (3, 3), 1)
+    #? count frames
+    if capture.get(cv.CAP_PROP_POS_FRAMES) == capture.get(cv.CAP_PROP_FRAME_COUNT):
+        capture.set(cv.CAP_PROP_POS_FRAMES, 0)
+    #? read video
+    success, img = capture.read()
+    #? process video
+    imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) #? grayscale
+    imgBlur = cv.GaussianBlur(imgGray, (3, 3), 1) #? create a gaussian matrix for process
     # ? binary image
     imgTreshold = cv.adaptiveThreshold(
-        imgBlur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 25, 16)
-
-    checkParkingSpace()
-
-    for pos in posList:
-        cv.rectangle(img, pos, (pos[0]+width, pos[1]+height), (255, 0, 255), 2)
-
-    cv.imshow("imgBlur", imgBlur)
+        imgBlur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 25, 20)
+    imgMedian = cv.medianBlur(imgTreshold, 5)
+    kernel = np.ones((9, 9), np.uint8) #? matrix of ones to process
+    imgDilate = cv.dilate(imgMedian, kernel, iterations=1)
+    #? function at 16
+    checkParkingSpace(imgDilate)
     cv.imshow("image", img)
     cv.waitKey(1)
