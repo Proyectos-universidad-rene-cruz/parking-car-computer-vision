2fa8989ecb588811f32071ea0345d1931f2f3c72
Rene Cruz <renecruz.0711@gmail.com>
2023-09-18 00:13:18 -0600

    mouseclick -> adding rectangle section

--- ParkingSpacePicker.py
+++ ParkingSpacePicker.py
@@ -1,40 +1,43 @@
 import cv2
 import pickle
 
-# ? base image
-
 # ? size values
 width, height = 65, 30
 
 try: 
+    #? If CarParkPos exists -> edit the file with the previous rectangles added
     with open('CarParkPos', 'rb') as f:
-        posList = pickle.load(f)
+        positionList = pickle.load(f)
 except:
-    posList = []
+    #? There does not exist any CarParkPos -> create a new list 
+    positionList = [] 
 
-
+#? detect the clicks and inserts a rectangle 
 def mouseclick(events, x, y, flags, params):
+    #? leftbutton pressed -> inserts a new rectangle in the position (x, y)
     if events == cv2.EVENT_LBUTTONDOWN:
-        posList.append((x, y))
+        positionList.append((x, y))
+    #? right button pressed -> removes the selected rectangle in the position
     if events == cv2.EVENT_RBUTTONDOWN:
-        for i, pos in enumerate(posList):
+        for i, pos in enumerate(positionList):
             x1, y1 = pos
             if x1 < x < x1+width and y1 < y < y1+height:
-                posList.pop(i)
-                
+                positionList.pop(i)
+    #? writes the rectangles in the files named "CarParkPos"
     with open('CarParkPos', 'wb') as f:
-        pickle.dump(posList, f)
+        pickle.dump(positionList, f)
     
 
-
+#? Base image
 while True:
+    #? reference image -> it must be the same view of the video
     img = cv2.imread('parking_3.png')
-    # ? Creating rectangle for parking space
-    # cv2.rectangle(img, (10, 45), (85, 85), (255, 0, 255), 2)
-    for pos in posList:
+    #? print purple rectangles in the selected places
+    for pos in positionList:
         cv2.rectangle(
             img, pos, (pos[0]+width, pos[1]+height), (255, 0, 255), 2)
     # ? Diplsay the image
     cv2.imshow("image", img)
+    #? mouse function detection
     cv2.setMouseCallback("image", mouseclick)
     cv2.waitKey(1)
